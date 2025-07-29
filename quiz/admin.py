c7397ca8fbea04b1b0dff1c0da18725f13cc0986
3phase <pbozhinow@gmail.com>
2025-07-29 09:17:02 +0200

    Exposes models in admin page so that they're manageable

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quiz, Question, Answer, QuestionOption
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question_list')
+    list_filter = ('title',)
+    search_fields = ('title',)
+
+    def question_list(self, obj):
+        return ", ".join(q.text for q in obj.questions.all())
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('quiz', 'text')
+    list_filter = ('quiz',)
+    search_fields = ('text',)
+
+
+@admin.register(QuestionOption)
+class QuestionOptionAdmin(admin.ModelAdmin):
+    list_display = ('get_question_text', 'text', 'correct')
+    list_filter = ('question', 'text')
+    search_fields = ('text',)
+
+    def get_question_text(self, obj):
+        return obj.question.text
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('get_question_text', 'get_question_options')
+    list_filter = ('question',)
+
+    def get_question_text(self, obj):
+        return obj.question.text
+
+    def get_question_options(self, obj):
+        return ", ".join(answer.text for answer in obj.question_options.all())
--- quiz/models.py
+++ quiz/models.py
@@ -15,15 +15,9 @@ class Quiz(models.Model):
     managed_by = models.ManyToManyField(
         USER_MODEL,
         related_name='quizzes_managed',
-        blank=True
-    )
-
-    def __str__(self):
-        return self.title
+        blank=True)
 
     class Meta:
-        verbose_name = 'Quiz'
-        verbose_name_plural = 'Quizzes'
         ordering = ['pk']
 
 
@@ -49,30 +43,16 @@ class Question(models.Model):
         related_name='prev_questions'
     )
 
-    def __str__(self):
-        return self.text[:50]
-
     class Meta:
-        verbose_name = 'Question'
-        verbose_name_plural = 'Questions'
         ordering = ['pk']
 
 
 class QuestionOption(models.Model):
     text = models.CharField(max_length=255)
     correct = models.BooleanField(default=False)
-    question = models.ForeignKey(
-        Question,
-        on_delete=models.CASCADE,
-        related_name='options'
-    )
-
-    def __str__(self):
-        return self.text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, )
 
     class Meta:
-        verbose_name = 'Question Option'
-        verbose_name_plural = 'Question Options'
         ordering = ['pk']
 
 
@@ -92,58 +72,27 @@ class Invitation(models.Model):
         on_delete=models.CASCADE,
         related_name='invitations_received'
     )
-
-    def __str__(self):
-        return f"Invitation({self.quiz}, from={self.from_user}, to={self.to_user})"
+    accepted = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ('quiz', 'from_user', 'to_user')
-        verbose_name = 'Invitation'
-        verbose_name_plural = 'Invitations'
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(
-        Question,
-        on_delete=models.CASCADE,
-        related_name='answers'
-    )
-    question_options = models.ManyToManyField(
-        QuestionOption,
-        related_name='answers'
-    )
-    user = models.ForeignKey(
-        USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='answers'
-    )
-
-    def __str__(self):
-        return f"Answer by {self.user} to Q{self.question.id}"
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_options = models.ManyToManyField(QuestionOption)
+    user = models.ForeignKey(USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('question', 'user')
-        verbose_name = 'Answer'
-        verbose_name_plural = 'Answers'
 
 
 class Score(models.Model):
-    user = models.ForeignKey(
-        USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='scores'
+    user = models.ForeignKey(USER_MODEL, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    points = models.FloatField(
+        default=0,
     )
-    quiz = models.ForeignKey(
-        Quiz,
-        on_delete=models.CASCADE,
-        related_name='scores'
-    )
-    points = models.FloatField()
-
-    def __str__(self):
-        return f"Score({self.user}, {self.quiz}) = {self.points}"
 
     class Meta:
         unique_together = ('user', 'quiz')
-        verbose_name = 'Score'
-        verbose_name_plural = 'Scores'
