3111a093d2833b62eba757586bda0e55daecc604
3phase <pbozhinow@gmail.com>
2025-07-29 09:17:50 +0200

    API logic and urls; currently using user param in the url, can change wiht postman authentication if not too much effort

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,69 @@
 from django.urls import path
-from . import views
+
+from .views import QuizViewSet, InvitationViewSet, QuestionViewSet, AnswerViewSet, ScoreViewSet, ProgressViewSet
+
+quiz_list = QuizViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+quiz_detail = QuizViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+quiz_scores = QuizViewSet.as_view({'get': 'scores'})
+
+inv_list = InvitationViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+inv_detail = InvitationViewSet.as_view({
+    'get': 'retrieve',
+    'delete': 'destroy'
+})
+
+q_list = QuestionViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+q_detail = QuestionViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+ans_list = AnswerViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+ans_detail = AnswerViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+score_detail = ScoreViewSet.as_view({
+    'get': 'list',
+})
 
 urlpatterns = [
-    path('', views.api_home, name='index'),
+    path('users/<int:user_pk>/quizzes/', quiz_list, name='quiz-list'),
+    path('users/<int:user_pk>/quizzes/<int:pk>/', quiz_detail, name='quiz-detail'),
+    path('users/<int:user_pk>/quizzes/<int:pk>/scores/', quiz_scores, name='quiz-scores'),
+    path('users/<int:user_pk>/invitations/', inv_list, name='inv-list'),
+    path('users/<int:user_pk>/invitations/<int:pk>/', inv_detail, name='inv-detail'),
+    path('users/<int:user_pk>/questions/', q_list, name='question-list'),
+    path('users/<int:user_pk>/questions/<int:pk>/', q_detail, name='question-detail'),
+    path('users/<int:user_pk>/answers/', ans_list, name='answer-list'),
+    path('users/<int:user_pk>/answers/<int:pk>/', ans_detail, name='answer-detail'),
+    path('users/<int:user_pk>/scores/', score_detail, name='score-detail'),
+    path('users/<int:user_pk>/quizzes/<int:quiz_pk>/progress/', ProgressViewSet.as_view(), name='quiz-progress'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,99 @@
-import json
-from django.http import JsonResponse
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from rest_framework import viewsets, views
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
+from .models import Quiz, Score, Invitation, Question, Answer
+from .permissions import IsQuizCreatorManagerOrInvitee
+from .serializers import QuizSerializer, ScoreSerializer, InvitationSerializer, QuestionSerializer, AnswerSerializer
 
-# Create your views here.
+User = get_user_model()
 
-@api_view(['POST'])
-def api_home(request, *args, **kwargs):
-    data = {}
-    return Response(data)
+
+class QuizViewSet(viewsets.ModelViewSet):
+    serializer_class = QuizSerializer
+
+    permission_classes = [IsQuizCreatorManagerOrInvitee]
+
+    def get_user(self):
+        return User.objects.get(pk=self.kwargs['user_pk'])
+
+    def get_queryset(self):
+        user = self.get_user()
+        return Quiz.objects.filter(
+            Q(created_by=user) |
+            Q(managed_by=user)
+        ).distinct()
+
+    @action(detail=True, methods=['get'])
+    def scores(self, request, user_pk=None, pk=None):
+        qs = Score.objects.filter(quiz_id=pk)
+        return Response(ScoreSerializer(qs, many=True).data)
+
+
+class InvitationViewSet(viewsets.ModelViewSet):
+    serializer_class = InvitationSerializer
+
+    def get_user(self):
+        return User.objects.get(pk=self.kwargs['user_pk'])
+
+    def get_queryset(self):
+        # invitations sent to me
+        return Invitation.objects.filter(to_user=self.get_user())
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.select_related('quiz').prefetch_related('options')
+    serializer_class = QuestionSerializer
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+    def get_user(self):
+        return User.objects.get(pk=self.kwargs['user_pk'])
+
+    def perform_create(self, serializer):
+        ans = serializer.save(user=self.get_user())
+        correct_count = ans.question_options.filter(correct=True).count()
+        score, _ = Score.objects.get_or_create(
+            quiz=ans.question.quiz, user=self.get_user()
+        )
+        score.points += correct_count
+        score.save()
+
+
+class ScoreViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ScoreSerializer
+
+    def get_user(self):
+        return User.objects.get(pk=self.kwargs['user_pk'])
+
+    def get_queryset(self):
+        return Score.objects.filter(user=self.get_user())
+
+
+class ProgressViewSet(views.APIView):
+    def get_user(self):
+        return get_object_or_404(User, pk=self.kwargs['user_pk'])
+
+    def get_quiz(self):
+        return get_object_or_404(Quiz, pk=self.kwargs['quiz_pk'])
+
+    def get(self, request, user_pk, quiz_pk, *args, **kwargs):
+        user = self.get_user()
+        quiz = self.get_quiz()
+
+        total_answers = Answer.objects.filter(
+            user=user,
+            question__quiz=quiz
+        ).count()
+
+        quiz_questions = Question.objects.filter(
+            quiz=quiz
+        ).count()
+
+        return Response({"completion": 100 * total_answers / quiz_questions})
